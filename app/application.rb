88d5c5b9f4c4d434adac7857fc67d1c3c676f688
grandballoon <luke.sovern.conley@gmail.com>
2018-07-16 11:00:33 -0400

    did it

---
+++ app/application.rb
@@ -0,0 +1,52 @@
+require 'pry'
+class Application
+
+  @@items = [Item.new("figs", 3.50), Item.new("apples", 1.20)]
+
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path.match(/items/)
+
+    item_name = req.path.split("/items/").last
+
+  item = @@items.find{|i| i.name == item_name}
+    if item
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+    end
+  else
+    resp.write "Route not found"
+    resp.status = 404
+  end
+ resp.finish
+end
+
+end
+
+# class Application
+#
+#   @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+#
+#     if req.path.match(/items/)
+#       item_name = req.path.split("/items/").last
+#       if item =@@items.find{|i| i.name == item_name}
+#         resp.write item.price
+#       else
+#         resp.status = 400
+#         resp.write "Item not found"
+#       end
+#     else
+#       resp.status=404
+#       resp.write "Route not found"
+#     end
+#     resp.finish
+#   end
+#
+# end
